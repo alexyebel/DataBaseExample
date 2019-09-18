7097f872131f93725435cd657ec58e076187f84f
alexyebel <36329272+alexyebel@users.noreply.github.com>
2019-09-18 15:22:40 +0300

    add data to dataBase and read data from dataBase

--- app/src/main/java/com/example/databaseexample/MainActivity.java
+++ app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.databaseexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -24,8 +27,11 @@ public class MainActivity extends AppCompatActivity {
         database=openOrCreateDatabase("database.sql",MODE_PRIVATE,null);
         database.execSQL(CREATE_TABLE_CMD);
 
+        final ListView listView=findViewById(R.id.persons_list);
 
-        ListView listView=findViewById(R.id.persons_list);
+        final ArrayAdapter<String> adapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
+
+        listView.setAdapter(adapter);
 
         final EditText firstEt = findViewById(R.id.first_input);
         final EditText lastEt = findViewById(R.id.last_input);
@@ -34,7 +40,16 @@ public class MainActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String first=firstEt.getText().toString();
+                String last=lastEt.getText().toString();
 
+                ContentValues contentValues=new ContentValues();
+                contentValues.put("first",first);
+                contentValues.put("last",last);
+
+                database.insert(TABLE_NAME,null,contentValues);
+                firstEt.setText("");
+                lastEt.setText("");
             }
         });
 
@@ -44,6 +59,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                adapter.clear();
+                //Cursor is a pointer in table
+                Cursor cursor=database.query(TABLE_NAME,null,null,null,null,null,null);
+                int firstNameIndex=cursor.getColumnIndex("first");
+                int lastNameIndex=cursor.getColumnIndex("last");
+
+                while (cursor.moveToNext()){
+                    String person=cursor.getString(firstNameIndex)+" "+cursor.getString(lastNameIndex)+"";
+
+                    adapter.add(person);
+                }
+                adapter.notifyDataSetChanged();
             }
         });
 
